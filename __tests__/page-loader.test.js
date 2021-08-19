38c06de7a0e6b6c118699aa7f46ed4730cda48d2
Anton Yakutovich <drakulavich@gmail.com>
2021-08-19 23:18:48 +0400

    Throw generic errors in tests

--- __tests__/page-loader.test.js
+++ __tests__/page-loader.test.js
@@ -6,8 +6,6 @@ import crypto from 'crypto';
 import path from 'path';
 import os from 'os';
 import loadPage from '../src/page-loader.js';
-import FsAccessError from '../src/errors/fs-error.js';
-import ResourceAccessError from '../src/errors/resource-error.js';
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
@@ -84,15 +82,7 @@ describe('page-loader', () => {
       .reply(200);
 
     const notExistingPath = path.join(tempPath, 'not_existing_dir');
-    await expect(loadPage('http://test.com/books', notExistingPath)).rejects.toThrow(FsAccessError);
-  });
-
-  it('should handle main page access errors', async () => {
-    nock('http://example.com')
-      .get('/users')
-      .reply(404);
-
-    await expect(loadPage('http://example.com/users', tempPath)).rejects.toThrow(ResourceAccessError);
+    await expect(loadPage('http://test.com/books', notExistingPath)).rejects.toThrow();
   });
 
   it('should handle resource access errors', async () => {
@@ -102,23 +92,11 @@ describe('page-loader', () => {
       .replyWithFile(200, getFixturePath('hexlet-courses.html'), {
         'Content-Type': 'text/html; charset=UTF-8',
       })
-      .get('/assets/professions/nodejs.png')
-      .replyWithFile(200, getFixturePath('nodejs.png'), {
-        'Content-Type': 'image/png',
-      })
-      .get('/assets/testing/pyramid.jpeg')
-      .replyWithFile(401, getFixturePath('pyramid.jpeg'), {
-        'Content-Type': 'image/jpeg',
-      })
-      .get('/assets/application.css')
-      .replyWithFile(404, getFixturePath('application.css'), {
-        'Content-Type': 'text/css',
-      })
       .get('/packs/js/runtime.js')
       .replyWithFile(500, getFixturePath('runtime.js'), {
         'Content-Type': 'text/javascript',
       });
 
-    await expect(loadPage('https://ru.hexlet.io/courses', tempPath)).rejects.toThrow(ResourceAccessError);
+    await expect(loadPage('https://ru.hexlet.io/courses', tempPath)).rejects.toThrow();
   });
 });
